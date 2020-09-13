b41e9637a364c099f89fa8e9d4860521d8c0d3c4
mmesihovic <mmesihovic1@etf.unsa.ba>
2020-09-13 22:30:21 +0200

    Modified db password

--- src/utils/dbconfig.js
+++ src/utils/dbconfig.js
@@ -2,7 +2,7 @@ export const config = {
     host: 'localhost',
     user: 'postgres',
     database: 'uup-game',
-    password: '',
+    password: 'postgres',
     port: 5432,
     max: 20
     //idleTimeoutMillis: 
